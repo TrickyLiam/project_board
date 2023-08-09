5301bcd422d2d7a1d672de81367f7e6a287985bf
TrickyLiam <liamcarlson2014@gmail.com>
2023-08-08 19:57:11 -0500

    variable change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function rating(){
     const select = document.getElementById("rating");
-    for (var i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++){
     select.options[select.options.length] = new Option(i+1, i);
   }
 }
