cf566bce6f3e8274876e19d9b495f9bd9b7a20bd
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-08-14 18:00:23 -0500

    add comments for btn functionality

--- renderCards.js
+++ renderCards.js
@@ -6,12 +6,12 @@ export function renderCards(arr) {
     const cardElement = document.createElement("div");
     cardElement.classList.add("card");
     cardElement.innerHTML = `
-        <button type="button" id="delete-btn" onclick="return this.parentNode.remove();">x</button>
+        <button type="button" id="delete-btn" onclick="return this.parentNode.remove()">x</button>
         <h1>${card.projectName}</h1>
         <p>${card.projectDescription}</p>
         <p>${card.projectRating}</p>
       `;
     boardContainer.appendChild(cardElement);
-    
   });
 }
+// button deletes it from view but saves in memory so if user resubmits card itll populate two instead of one if one was already filled out. will need to fix.
