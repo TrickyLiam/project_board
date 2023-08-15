dc39491e41d091b9847769ae649c7bf35465b47c
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-08-15 14:52:47 -0500

    .

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ window.addEventListener("load", function () {
   const ratingButton = document.getElementById("rating");
   let cardSubmittedArr = [];
 
-  ratingButton.addEventListener("click", (e) => {
+  ratingButton.addEventListener(
+    "click",
+    (e) => {
       rating();
-    }, { once: true }
+    },
+    { once: true }
   );
 
   // const del = document.getElementById("delete-btn");
@@ -19,24 +22,29 @@ window.addEventListener("load", function () {
   // })
   // const deleteBtn= document.getElementById("delete-btn");
   // const boardContainer = document.getElementById("board-container");
-  
-  
+
   // deleteBtn.addEventListener("click", function() {
   //   boardContainer.remove();
   // })
   projectForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    
     const projectName = document.querySelector("input[name=project]").value;
-    const projectDescription = document.querySelector("input[name=description]").value;
-    const projectRating = Number(document.querySelector("select[name=rating]").value);
-    
-    
-    let isFormValid = formValidation(projectName, projectDescription, projectRating);
+    const projectDescription = document.querySelector(
+      "input[name=description]"
+    ).value;
+    const projectRating = Number(
+      document.querySelector("select[name=rating]").value
+    );
+
+    let isFormValid = formValidation(
+      projectName,
+      projectDescription,
+      projectRating
+    );
 
     if (isFormValid) {
-      let cardToPush = { projectName, projectDescripton, projectRating };
+      let cardToPush = { projectName, projectDescription, projectRating };
       cardSubmittedArr.push(cardToPush);
       renderCards(cardSubmittedArr);
     }
